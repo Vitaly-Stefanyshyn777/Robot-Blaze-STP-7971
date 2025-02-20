7b36e558d062b444b4d44a8ccc5d19fef7e31b24
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-02-20 21:19:39 +0200

    допрацював About game

--- src/js/gallery-swiper.js
+++ src/js/gallery-swiper.js
@@ -1,28 +1,19 @@
-import Swiper from 'swiper';
-import {
-  Navigation,
-  Pagination,
-  Keyboard,
-  EffectCreative,
-} from 'swiper/modules';
+import Swiper from "swiper";
+import { Navigation, Pagination, Keyboard } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/effect-creative';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-const swiperGallery = new Swiper('.swiper-gallery', {
-  direction: 'horizontal',
+const swiperGallery = new Swiper(".swiper-gallery", {
+  direction: "horizontal",
   slidesPerView: 1,
   spaceBetween: 0,
   initialSlide: 0,
   loop: true,
   slideToClickedSlide: true,
-  slideClass: 'swiper-slide-gallery',
-  wrapperClass: 'swiper-wrapper-gallery',
-
-  effect: window.innerWidth <= 1200 ? 'slide' : 'creative',
+  slideClass: "swiper-slide-gallery",
+  wrapperClass: "swiper-wrapper-gallery",
 
   keyboard: {
     enabled: true,
@@ -30,35 +21,26 @@ const swiperGallery = new Swiper('.swiper-gallery', {
   },
 
   pagination: {
-    el: '#gallery-pagination',
-    bulletClass: 'pagination-btn',
-    bulletActiveClass: 'pagination-btn--active',
+    el: "#gallery-pagination",
+    bulletClass: "pagination-btn",
+    bulletActiveClass: "pagination-btn--active",
     clickable: true,
   },
 
-  modules: [Navigation, Keyboard, Pagination, EffectCreative],
+  modules: [Navigation, Keyboard, Pagination],
 
   navigation: {
-    nextEl: '#gallery-btn-next',
-    prevEl: '#gallery-btn-prev',
+    nextEl: "#gallery-btn-next",
+    prevEl: "#gallery-btn-prev",
   },
 
   breakpoints: {
     1200: {
-      slidesPerView: 'auto',
+      slidesPerView: 1, // Рівно один слайд
       spaceBetween: 10,
-      centeredSlides: true,
-      effect: 'creative',
-      creativeEffect: {
-        limitProgress: 2,
-        prev: {
-          translate: [-300, '10%', 0],
-        },
-        next: {
-          translate: [300, '10%', 0],
-        },
-      },
-      slideActiveClass: 'gallery-active-slide',
+      centeredSlides: false, // Вимикаємо центрування, щоб не було накладання
+      effect: "slide", // Простий ефект, без creative налаштувань
+      slideActiveClass: "gallery-active-slide",
     },
     320: {
       slidesPerView: 1,
